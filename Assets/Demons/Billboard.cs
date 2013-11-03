c50c27da08be8ef7b99d7f6267a49b7e0d078a8b
unknown <wilbefast@gmail.com>
2013-11-03 00:53:39 -0700

    basic 3 demons

---
+++ Assets/Demons/Billboard.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Billboard : MonoBehaviour 
+{
+	void Start () 
+	{
+		if(transform.forward.x < 0)
+			// invert the x scale so that the billboard face away from Z
+			transform.localScale = new Vector3(
+				-transform.localScale.x, 
+				transform.localScale.y, 
+				transform.localScale.z);
+	}
+}
