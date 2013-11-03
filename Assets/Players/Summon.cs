68d777f9d97416410ee2c8448fb2be715c043eec
unknown <wilbefast@gmail.com>
2013-11-02 22:46:23 -0700

    initial dump

---
+++ Assets/Players/Summon.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Summon : MonoBehaviour 
+{
+	[System.Serializable]
+	public class Option
+	{
+		public GameObject prefab;
+		public KeyCode keyboardKey;
+	}
+	public Option[] options;
+	
+	
+	void Update () 
+	{
+		foreach(Option o in options)
+		{
+			if(Input.GetKeyDown(o.keyboardKey))
+			{
+				GameObject minion = (GameObject)GameObject.Instantiate(
+					o.prefab, transform.position, transform.rotation);
+			}
+		}
+	}
+}
